7e41fa3e9dddce4c204af464b5af856fe98c5a56
José Armando Cid González <144040412+Josse66@users.noreply.github.com>
2026-09-18 18:12:20 +0000

    ✨ Feat: Actualiza app.js, www, package.json
    Enhance app.js With middleware configuration and error handling comments; update www for debug message; add greet script in package.json; include fondo de pantalla.jpg

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
+//funcion para manejar errores
 var createError = require('http-errors');
+//importa el framework express
 var express = require('express');
+//imporeta modulos para importar rutas
 var path = require('path');
+//importa modulos para manejar cookies
 var cookieParser = require('cookie-parser');
+//importa modulos para manejar logs
 var logger = require('morgan');
 
+//importa las rutas de la aplicacion
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//crea la aplicacion de express
 var app = express();
 
-// view engine setup
+// view engine setup, configura el motor de vistas
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+//configurar middelwares de la aplicacion
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
