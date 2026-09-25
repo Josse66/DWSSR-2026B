ac92515940ca24941b5ab3e6b3b4cb3f6063319a
José Armando Cid González <144040412+Josse66@users.noreply.github.com>
2026-09-25 18:55:59 +0000

    ✨FEAT: Nodemon
     Update Nodemon usage and enhance user route response

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('<h1 style = "color: blue; background-color: lightblue; font-size: 45px">LISTA DE USUARIOS</h1>');
 });
 
 export default router;
